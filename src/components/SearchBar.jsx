d14502a07f1b1fa588796be8741a0b955d623ce3
trendmanishGIT <trendymanish@gmail.com>
2023-03-27 23:46:30 +0530

    website completed

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,30 +1,48 @@
-import React from "react";
-import { Paper, IconButton } from "@mui/material";
-import { useState } from "react";
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { Search } from "@mui/icons-material";
-import { borderRadius } from "@mui/system";
+import { Paper, IconButton } from "@mui/material";
+import SearchIcon from "@mui/icons-material/Search";
 
 const SearchBar = () => {
+  const [searchTerm, setSearchTerm] = useState("");
+  const navigate = useNavigate();
+
+  const onhandleSubmit = (e) => {
+    e.preventDefault();
+
+    if (searchTerm) {
+      navigate(`/search/${searchTerm}`);
+
+      setSearchTerm("");
+    }
+  };
+
   return (
-    <div>
-      <form onSubmit={() => {}}>
-        <input
-          style={{
-            border: "1px solid",
-            borderRadius: "15px",
-            padding: "5px 5px",
-          }}
-          type="text"
-          placeholder="Search..."
-          value=""
-          onChange={() => {}}
-        />
-        <IconButton type="submit" sx={{ p: "10px", color: "red" }}>
-          <Search />
-        </IconButton>
-      </form>
-    </div>
+    <Paper
+      component="form"
+      onSubmit={onhandleSubmit}
+      sx={{
+        borderRadius: 20,
+        border: "1px solid #e3e3e3",
+        pl: 2,
+        boxShadow: "none",
+        mr: { sm: 5 },
+      }}
+    >
+      <input
+        className="search-bar"
+        placeholder="Search..."
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
+      />
+      <IconButton
+        type="submit"
+        sx={{ p: "10px", color: "red" }}
+        aria-label="search"
+      >
+        <SearchIcon />
+      </IconButton>
+    </Paper>
   );
 };
 
--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -1,11 +1,24 @@
 import { Box, Stack } from "@mui/material";
 import React from "react";
+import VideoCard from "./VideoCard";
+import ChannelCard from "./ChannelCard";
 
-const Videos = ({ videos }) => {
+const Videos = ({ videos, direction }) => {
+  if (!videos?.length) return "Loading...";
   return (
-    <Stack direction="row" flexWrap="wrap" justifyContent="start" gap={2}>
+    <Stack
+      direction={direction || "row"}
+      flexWrap="wrap"
+      justifyContent="start"
+      gap={2}
+    >
       {videos.map((item, index) => {
-        <Box>{item.videoId}</Box>;
+        return (
+          <Box key={index}>
+            {item.id.videoId && <VideoCard video={item} />}
+            {item.id.channelId && <ChannelCard channelDetail={item} />}
+          </Box>
+        );
       })}
     </Stack>
   );
